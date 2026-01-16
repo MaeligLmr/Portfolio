ce62ddf1f992893e8172dada04a9c9ef5838d0a3
Lamarre <maelig.lamarre49@gmail.com>
2026-01-16 22:40:48 +0100

    [FIX] - Fixed Path

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,7 +1,7 @@
 const CV = ({color, language}) => {
     return (
         <a className = "w-full p-3 text-lg font-semibold text-center text-white rounded-xl cursor-pointer transition duration-300 ease-in-out hover:bg-indigo-600 active:bg-indigo-900" 
-        href={language === "fr" ? "/docs/Maelig-LAMARRE-CV.pdf" : "docs/International-cv-maelig-lamarre.pdf"}
+        href={language === "fr" ? "/Portfolio/docs/Maelig-LAMARRE-CV.pdf" : "/Portfolio/docs/International-cv-maelig-lamarre.pdf"}
         download="Maelig-LAMARRE-CV.pdf"
          style={{backgroundColor : color,                
             color : color === '#FFD900' ? 'black' : 'white'
